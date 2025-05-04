d7060aa01e6f5be3a466b57764e2350ac4718516
ZPINXD <geovanogalan@gmail.com>
2025-05-04 21:25:41 +0700

    View data

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,2 +1,3 @@
-<a {{ $attributes }}
-class="rounded-md  px-3 py-2 text-sm font-medium {{ $active ? 'text-white bg-gray-900 ' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}  " aria-current="{{ $active ?'page' : false}}">{{ $slot }}</a>
+@props(['active' => false])
+<a 
+class="rounded-md  px-3 py-2 text-sm font-medium {{ $active ? 'text-white bg-gray-900 ' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}  " aria-current="{{ $active ?'page' : false}}" {{ $attributes }}>{{ $slot }}</a>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,9 +11,50 @@ Route::get('/About', function () {
     return view('About',['nama'=>'Vano','title'=> 'About']);
 });
 
-Route::get('/Blog', function () {
-    return view('Blog', ['title'=>'blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title'=>'Blog','posts'=> [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author'=> 'Geovano',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, asperiores veritatis ab itaque repellendus eius fugiat, quam eaque ipsum culpa soluta. Impedit ducimus maiores soluta optio nisi molestiae ipsa repellendus!'
+        ],
+        [
+            'id' => 2, 
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author'=> 'Geovano',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex deleniti saepe dolorem voluptatibus repudiandae explicabo iste inventore similique labore eveniet culpa ipsum, esse excepturi optio, deserunt nihil quas id nostrum.'
+        ]
+    ]]);
 });
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author'=> 'Geovano',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, asperiores veritatis ab itaque repellendus eius fugiat, quam eaque ipsum culpa soluta. Impedit ducimus maiores soluta optio nisi molestiae ipsa repellendus!'
+        ],
+        [
+            'id' => 2, 
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author'=> 'Geovano',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex deleniti saepe dolorem voluptatibus repudiandae explicabo iste inventore similique labore eveniet culpa ipsum, esse excepturi optio, deserunt nihil quas id nostrum.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use($slug){
+        return $post['slug'] == $slug;
+    });
+    return view('post',['title'=>'Single Post','post'=>$post]);
+
+});
+
 Route::get('/Contact', function () {
     return view('Contact',['title'=> 'Contact']);
 });
